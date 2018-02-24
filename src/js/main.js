14d1343c83aa9542e89291d7ea7d48d87a60027b
Des Barrett <des.barrett@gmail.com>
2018-02-24 15:42:29 +0000

    added axes labels

--- src/js/main.js
+++ src/js/main.js
@@ -1,5 +1,6 @@
 "use strict";
 
+
 function _defineProperty(obj, key, value) {
   if (key in obj) {
     Object.defineProperty(obj, key, { value: value, enumerable: true, configurable: true, writable: true });
@@ -71,7 +72,13 @@ function getData(url, callback) {
             ticks: {
               beginAtZero: true
             }
-          }]
+          }],
+            xAxes: [{
+              scaleLabel: {
+                display: true,
+                labelString: obj.description
+              },
+            }]
         }
       }
     });
